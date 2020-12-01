5c35f6709460e360e647bbcec8fd3cae6cbc8013
Seth Gower <sethzerish@gmail.com>
2020-12-01 14:26:36 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Advent of Code 2020
+
+This is my repository for my solutions for [Advent of
+Code](https://adventofcode.com/) 2020. This is the first time I am doing this
+competition, and have decided to take this opportunity to advance my rust
+skills. I am using a template created by @Scoder12, located on
+[repl.it](https://repl.it/@Scoder12/aoc-rust-template). 
---
+++ src/day01.rs
@@ -0,0 +1,26 @@
+pub fn part1(inp: String) {
+    let v : Vec<&str> = inp.split('\n').collect();
+    let mut int : Vec<i32> = Vec::new();
+    for number in v.iter() {
+        let temp = match number.parse::<i32>() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        int.push(temp);
+    }
+
+    let mut done = false;
+    for num in int.iter() {
+        for num1 in int.iter() {
+            if num + num1 == 2020 {
+                println!("The numbers were: {} and {}", num, num1);
+                println!("{}", num * num1);
+                done = true;
+                break;
+            }
+        }
+        if done {
+            break;
+        }
+    }
+}
