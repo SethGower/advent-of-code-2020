5b8c6863e8c7bcfde91197db19e159cdee71e093
Seth Gower <sethzerish@gmail.com>
2020-12-01 16:02:29 -0500

    Day 1 Part 2 done. But it is _gross_. Want to make more optimized

--- src/day01.rs
+++ src/day01.rs
@@ -24,3 +24,35 @@ pub fn part1(inp: String) {
         }
     }
 }
+
+pub fn part2(input: String) {
+    let v: Vec<&str> = input.split('\n').collect();
+    let mut int: Vec<i32> = Vec::new();
+    for number in v.iter() {
+        let temp = match number.parse::<i32>() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        int.push(temp);
+    }
+
+    let mut done = false;
+    for num in int.iter() {
+        for num1 in int.iter() {
+            for num2 in int.iter() {
+                if num + num1 + num2 == 2020 {
+                    println!("The numbers were: {}, {}, and {}", num, num1, num2);
+                    println!("{}", num * num1 * num2);
+                    done = true;
+                    break;
+                }
+            }
+            if done {
+                break;
+            }
+        }
+        if done {
+            break;
+        }
+    }
+}
