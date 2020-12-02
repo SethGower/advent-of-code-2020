f7497f69520be90606b09d344ebb5396d3b13038
Seth Gower <sdg7234@rit.edu>
2020-12-02 01:18:21 -0500

    Adds Solution for Part2 of Day 2
    this isn't very well written, but I am tired, and am still learning Rust

--- src/day02.rs
+++ src/day02.rs
@@ -44,3 +44,51 @@ pub fn part1(input: String) {
     }
     println!("Number of Valid Passwords: {}", count);
 }
+
+pub fn part2(input: String) {
+    fn process_entry(input: &str) -> bool {
+        let tokens: Vec<&str> = input.splitn(2, ' ').collect();
+
+        let mut bounds: [usize; 2] = [0, 0];
+
+        if let Some(string) = tokens.get(0) {
+            let bounds_in: Vec<&str> = string.split('-').collect();
+            if let Some(string) = bounds_in.get(0) {
+                if let Ok(num) = string.parse::<usize>() {
+                    bounds[0] = num;
+                }
+            }
+            if let Some(string) = bounds_in.get(1) {
+                if let Ok(num) = string.parse::<usize>() {
+                    bounds[1] = num;
+                }
+            }
+        };
+        if let Some(string) = tokens.get(1) {
+            let s = String::from(*string).trim().replace(" ", "");
+            let tokens: Vec<&str> = s.split(':').collect();
+            if let Some(string) = tokens.get(0) {
+                if let Some(password) = tokens.get(1) {
+                    let string = String::from(*string);
+                    let password = String::from(*password);
+
+                    let first = password.get(bounds[0]-1..bounds[0]).unwrap();
+                    let second = password.get(bounds[1]-1..bounds[1]).unwrap();
+
+                    return (string == first) ^ (string == second);
+                }
+            }
+        }
+
+        return false;
+    }
+    let lines: Vec<&str> = input.split('\n').collect();
+
+    let mut count: usize = 0;
+    for entry in lines {
+        if process_entry(entry) {
+            count = count + 1;
+        }
+    }
+    println!("Number of Valid Passwords: {}", count);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ pub type DayFn = fn(String);
 pub fn get_day(day: u32) -> (DayFn, DayFn) {
     return match day {
         1 => (day01::part1, day01::part2),
-        2 => (day02::part1, noop),
+        2 => (day02::part1, day02::part2),
         _ => {
             println!("Unknown day: {}", day);
             return (noop, noop);
