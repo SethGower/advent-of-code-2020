6ee4714329e75bf907a846ec94dfc0f7f7dd7d13
Seth Gower <sethzerish@gmail.com>
2020-12-03 16:47:59 -0500

    Part 1 of day 3 done

--- src/day03.rs
+++ src/day03.rs
@@ -1,8 +1,25 @@
-pub fn part1(input: String){
-    let matrix : Vec<&str> = input.split('\n').collect();
-    let numCol = match matrix.get(0) {
+pub fn part1(input: String) {
+    let matrix: Vec<&str> = input.split('\n').collect();
+    let num_col = match matrix.get(0) {
         Some(row) => row.len(),
-        _ => 0
+        _ => 0,
     };
+    let mut num_trees: usize = 0;
+    let mut curr_col: usize = 0;
 
+    for row in matrix.iter() {
+        if row.len() > 0 {
+            curr_col = curr_col % num_col;
+            let row_s = String::from(*row);
+            if let Some(character) = row_s.get(curr_col..curr_col+1){
+                if character == "#" {
+                    num_trees += 1;
+                }
+            }
+            curr_col += 3;
+        } else {
+            break;
+        }
+    }
+    println!("Trees: {}", num_trees);
 }
