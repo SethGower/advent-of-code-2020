58309c78af86964551508df316948d5a996d44a5
Seth Gower <sethzerish@gmail.com>
2020-12-09 11:02:23 -0500

    Finishes day 8

--- src/day08.rs
+++ src/day08.rs
@@ -1,94 +1,80 @@
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 enum OpCode {
     Acc,
     Jmp,
     Nop,
 }
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Instruction {
     op: OpCode,
     val: i32,
-    runs: i32,
 }
 
 pub fn part1(input: String) -> Option<String> {
-    let mut instructions: Vec<Instruction> = input
+    let instructions: Vec<Instruction> = input
         .lines()
         .map(|line| parse_instruction(line).unwrap())
         .collect();
 
-    let mut acc: i32 = 0;
-    let mut i: i32 = 0;
-    while i < instructions.len() as i32 {
-        if let Some(mut inst) = instructions.get_mut(i as usize) {
-            if inst.runs > 0 {
-                break;
-            }
-            inst.runs += 1;
-            i += 1;
-            match inst.op {
-                OpCode::Nop => continue,
-                OpCode::Acc => acc += inst.val,
-                OpCode::Jmp => i += inst.val - 1,
-            }
-            if i < 0 {
-                panic!("Program counter went below zero");
-            }
-        }
-    }
-    println!("i: {}", i);
+    let (finished, acc) = run_instructions(instructions);
+    println!("Successfully Finished? : {:?}", finished);
     println!("Accumulator: {}", acc);
     Some(acc.to_string())
 }
 
+fn exec_instruction(inst: &Instruction, acc: &mut i32, prog_counter: &mut usize) {
+    match inst.op {
+        OpCode::Nop => *prog_counter += 1,
+        OpCode::Acc => {
+            *acc += inst.val;
+            *prog_counter += 1;
+        }
+        OpCode::Jmp => {
+            *prog_counter = (*prog_counter as i32 + inst.val) as usize;
+        }
+    }
+}
+fn run_instructions(prog: Vec<Instruction>) -> (bool, i32) {
+    let mut inst_run = vec![false; prog.len()];
+    let mut acc: i32 = 0;
+    let mut i: usize = 0;
+    while i < prog.len() {
+        let inst = &prog[i];
+        if inst_run[i] {
+            break;
+        }
+        inst_run[i] = true;
+        exec_instruction(inst, &mut acc, &mut i);
+    }
+
+    if i == prog.len() {
+        (true, acc)
+    } else {
+        (false, acc)
+    }
+}
+
 pub fn part2(input: String) -> Option<String> {
-    let mut instructions: Vec<Instruction> = input
+    let instructions: Vec<Instruction> = input
         .lines()
         .map(|line| parse_instruction(line).unwrap())
         .collect();
 
-    let mut acc: i32 = 0;
-    let mut i: i32 = 0;
-    while i < instructions.len() as i32 {
-        if let Some(inst) = instructions.get_mut(i as usize) {
-            if inst.runs > 0 {
-                println!("{:?}", inst);
-                break;
-            }
-            i += 1;
-            inst.runs += 1;
-            match inst.op {
-                OpCode::Nop => {
-                    // if let Some(next) = instructions.get(i as usize){
-                    //     if next.runs > 0 {
-                    //         i -= 1;
-                    //         inst.op = OpCode::Jmp;
-                    //     }else{
-                    //         continue;
-                    //     }
-                    // }
-                }
-                OpCode::Acc => acc += inst.val,
-                OpCode::Jmp => {
-                    i += inst.val - 1;
-                }
-            }
-            if i < 0 {
-                panic!("Program counter went below zero");
-            }
+    for i in 0..instructions.len() {
+        let mut program = instructions.clone();
+        match program[i].op {
+            OpCode::Nop => program[i].op = OpCode::Jmp,
+            OpCode::Jmp => program[i].op = OpCode::Nop,
+            _ => continue,
         }
+        let (success, acc) = run_instructions(program);
+        if success {
+            println!("ACC: {}", acc);
+            return Some(acc.to_string());
+        }
+
     }
-
-    println!("{:#?}", instructions);
-    let run: Vec<Instruction> = instructions
-        .into_iter()
-        .filter(|x| x.runs > 0 && x.op != OpCode::Acc)
-        .collect();
-    println!("{:#?}", run);
-
-    println!("i: {}", i);
-    println!("Accumulator: {}", acc);
-    Some(acc.to_string())
+    None
 }
 
 fn parse_instruction(s: &str) -> Option<Instruction> {
@@ -99,7 +85,6 @@ fn parse_instruction(s: &str) -> Option<Instruction> {
             let instruction = Instruction {
                 op: OpCode::Nop,
                 val: 0,
-                runs: 0,
             };
             Some(instruction)
         }
@@ -107,7 +92,6 @@ fn parse_instruction(s: &str) -> Option<Instruction> {
             let instruction = Instruction {
                 op: OpCode::Acc,
                 val: val.parse::<i32>().ok()?,
-                runs: 0,
             };
             Some(instruction)
         }
@@ -115,7 +99,6 @@ fn parse_instruction(s: &str) -> Option<Instruction> {
             let instruction = Instruction {
                 op: OpCode::Jmp,
                 val: val.parse::<i32>().ok()?,
-                runs: 0,
             };
             Some(instruction)
         }
