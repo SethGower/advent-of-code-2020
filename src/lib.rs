8ac0a96770fdcf19cad1df0a12975f3e98b4b9f2
Seth Gower <sethzerish@gmail.com>
2020-12-09 10:24:28 -0500

    Moves the time functions to the lib so I can use it in the day files

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::time::Duration;
 // Days
 pub mod day01;
 pub mod day02;
@@ -31,4 +32,28 @@ pub fn get_day(day: u32) -> (DayFn, DayFn) {
             return (noop, noop);
         }
     };
+}
+
+fn fmt_time(ms: f64) -> String {
+    if ms <= 1.0 {
+        let micro_sec = ms * 1000.0;
+        return String::from(format!("{}µs", micro_sec.round()));
+    }
+    if ms < 1000.0 {
+        let whole_ms = ms.floor();
+        let rem_ms = ms - whole_ms;
+        return String::from(format!("{}ms ", whole_ms) + &fmt_time(rem_ms));
+    }
+    let sec: f64 = ms / 1000.0;
+    if sec < 60.0 {
+        let whole_sec = sec.floor();
+        let rem_ms = ms - whole_sec * 1000.0;
+        return format!("{}s ", whole_sec) + &fmt_time(rem_ms);
+    }
+    let min: f64 = sec / 60.0;
+    return format!("{}m ", min.floor()) + &fmt_time((sec % 60.0) * 1000.0);
+}
+
+pub fn fmt_dur(dur: Duration) -> String {
+    return fmt_time(dur.as_secs_f64() * 1000.0);
 }
