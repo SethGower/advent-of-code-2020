a0c9784ae85fe8647232d6817beb8ef8a389c9ad
Seth Gower <sethzerish@gmail.com>
2020-12-11 14:29:25 -0500

    committing what I have for day 10. Not finished yet

--- src/day10.rs
+++ src/day10.rs
@@ -1,5 +1,3 @@
-use itertools::Itertools;
-
 pub fn part1(input: String) -> Option<String> {
     let mut joltages: Vec<u32> = input.lines().map(|x| x.parse::<u32>().unwrap()).collect();
     joltages.sort();
@@ -22,33 +20,7 @@ pub fn part1(input: String) -> Option<String> {
 }
 
 pub fn part2(input: String) -> Option<String> {
-    let joltages: Vec<u32> = input.lines().map(|x| x.parse::<u32>().unwrap()).collect();
-    let max = joltages.iter().max()? + 3;
-    println!("{:#?}", joltages);
-    let mut valid: u32 = 0;
-    for i in 1..joltages.len() {
-        let joltages = joltages.clone();
-        for comb in joltages.iter().combinations(i) {
-            let mut comb = comb.clone();
-            comb.push(&max);
-            if is_valid(&mut comb, 0) {
-                valid += 1;
-            }
-        }
-    }
-    println!("Valid: {}", valid);
-    Some(input)
-}
-
-fn is_valid(joltages: &mut Vec<&u32>, input: u32) -> bool {
+    let mut joltages: Vec<u32> = input.lines().map(|x| x.parse::<u32>().unwrap()).collect();
     joltages.sort();
-    let mut prev_adapter: u32 = input;
-    for adapter in joltages.iter() {
-        if **adapter - prev_adapter > 3 {
-            return false;
-        }
-        prev_adapter = **adapter;
-    }
-    println!("{:?} is valid", joltages);
-    true
+    Some(input)
 }
